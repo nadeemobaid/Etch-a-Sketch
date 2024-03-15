9486022e64d14671d30dfb5c0db3dc1c5ef4cf26
obaidullah <123308676+nadeemobaid@users.noreply.github.com>
2024-03-15 17:13:24 +0530

    add userGrid -- customize the grid

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const configBtn = document.querySelector("button");
 const gridContainer = document.querySelector(".grid-container");
 
-function createGrid(col = 16, row = 16) {
-    for (let i = 1; i <= col; i++) {
+function createGrid(grid=16) {
+    for (let i = 1; i <= grid; i++) {
         const gridCol = document.createElement("div");
         gridCol.classList.add("grid-col");
         gridContainer.appendChild(gridCol);
 
-        for (let j = 1; j <= row; j++) {
+        for (let j = 1; j <= grid; j++) {
             const gridRow = document.createElement("div");
             gridRow.classList.add("grid-row");
             gridCol.appendChild(gridRow);
@@ -26,24 +26,18 @@ function removeGrid() {
     gridCols.forEach((div) => div.remove());
 }
 
+function userGrid(){
+    const gridSize = parseInt(prompt("Enter your desired GRID SIZE\nrange 1-10"));
+    if(gridSize < 1 || gridSize > 100){
+        alert("Please enter integer number 1-100 only")
+    } else {
+        removeGrid();
+        createGrid(gridSize)
+    };
+}
+
 createGrid();
 
-
-
-
-
-configBtn.addEventListener('click', () => {
-    let userCol = prompt("Enter column: \nlimit 100");
-    if (userCol > 100) {
-        userCol = prompt("Plese enter only integer Numbers range 1-100!!");
-        console.log(userCol);
-    }
-    let userRow = prompt("Enter row: \n limit 100");
-    if (userRow > 100) {
-        userRow = prompt("Plese enter only integer Numbers range 1-100!!");
-        console.log(userRow);
-    }
-
-})
+configBtn.addEventListener('click', userGrid);
 
 
